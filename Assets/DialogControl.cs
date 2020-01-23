2a0265a8edba416761e079f091105c5a56ffaf61
GonsalesAlisson <gonsalesalisson@usp.br>
2020-01-23 17:05:14 -0300

    Commit2

---
+++ Assets/DialogControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogControl : MonoBehaviour
+{
+    public static DialogControl instance;
+	void Awake(){
+		if(instance!=null){
+			Debug.LogError("Mais de um DialogControl");
+			return;
+		}
+
+		instance = this;
+	}
+	public Image panel;
+	public Text text;
+    void Start()
+    {
+        panel.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void showMessage(string message){
+    	panel.gameObject.SetActive(true);
+    	text.text = message;
+    }
+}
---
+++ Assets/Ground.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    // Start is called before the first frame update
+    DialogControl dialog;
+    void Start()
+    {
+        dialog = DialogControl.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col){
+    	dialog.showMessage("Enkidu, meu irmão. Vamos nos aventurar enquanto sonhas mais uma vez");
+    }
+}
